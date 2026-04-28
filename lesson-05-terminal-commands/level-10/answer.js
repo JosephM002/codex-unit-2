de845d065d0c3a50844b1499059e96de9b29960f
Joseph <corrymuth002@gmail.com>
2026-04-28 18:42:06 -0500

    moved a folder

--- lesson-05-terminal-commands/level-10/answer.js
+++ lesson-05-terminal-commands/level-10/answer.js
@@ -1,3 +1,6 @@
 // Level 10 — Challenge: Multi-step script run
 // TODO: Export an object like { steps: ["node ./folder/script.js"], notes: "moved file using VS Code" }
-export default { steps: [], notes: "" };
+export default {
+  steps: ["ls", "cd folder", "node ./folder/script.js"],
+  notes: "moved a file",
+};
