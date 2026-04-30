0d7c5aa55d021dbc4aca40220e37f869f47371a4
Joseph <corrymuth002@gmail.com>
2026-04-29 20:34:28 -0500

    modified lesson 6-11

--- lesson-06-js-syntax/level-11-function-calls/script.js
+++ lesson-06-js-syntax/level-11-function-calls/script.js
@@ -5,4 +5,15 @@ export function reportScore(playerName, score) {
   return playerName + " scored " + score + " points";
 }
 
-const avaScoreReport = reportScore("Ava", 0);
+const avaScoreReport = reportScore("Ava", 1);
+const rolazarScoreReport = reportScore("Rolazar", 0);
+const amberScoreReport = reportScore("Amber", 2);
+const ashantiScoreReport = reportScore("Ashanti", 3);
+const josephScoreReport = reportScore("Joseph", 4);
+const chrisScoreReport = reportScore("Christopher", 5);
+const donaldScoreReport = reportScore("Donald", 6);
+const kimScoreReport = reportScore("Kimberly", 7);
+const mariahScoreReport = reportScore("Mariah", 8);
+const pearlaScoreReport = reportScore("Pearla", 9);
+
+export default avaScoreReport;
