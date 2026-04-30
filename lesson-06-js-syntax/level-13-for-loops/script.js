94df56efe2fd3622cb82db273a8b876c1957ee58
Joseph <corrymuth002@gmail.com>
2026-04-29 20:34:48 -0500

    modified lesson 6-13

--- lesson-06-js-syntax/level-13-for-loops/script.js
+++ lesson-06-js-syntax/level-13-for-loops/script.js
@@ -1,2 +1,36 @@
 // Level 13 — `for` loops practice (10 different ranges)
+
+import isLoggedIn from "../level-04-booleans-variables/script";
+
 // TODO: Write 10 different for loops that print iterated numbers with console.log.
+export default "loops mode";
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+for (let i = 5; i > 5; i--) {
+  console.log(i);
+}
+for (let i = 20; i < 25; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 2; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 6; i + 2) {
+  console.log(i);
+}
+for (let i = 10; i > 10; i - 2) {
+  console.log(i);
+}
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+for (let i = 100; i > 100; i - 10) {
+  console.log(i);
+}
+for (let i = 3; i < 15; i + 3) {
+  console.log(i);
+}
