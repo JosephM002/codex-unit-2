43f10e55d06cadd5cacc6aac59baca194b5e39c5
Joseph <corrymuth002@gmail.com>
2026-04-29 20:31:05 -0500

    modified lesson 5-3

--- lesson-05-terminal-commands/level-03/answer.js
+++ lesson-05-terminal-commands/level-03/answer.js
@@ -1,3 +1,3 @@
 // Level 03 — Change directory with cd
 // TODO: Use `cd` to move into a folder and back; describe your commands here.
-export default "";
+export default "ls showed me folders, cd 'Saved Games' moved me into saved games folder";
