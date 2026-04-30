168e5c65c9f6de27e547f202da25eac831f3d1cd
Joseph <corrymuth002@gmail.com>
2026-04-29 20:33:56 -0500

    modified lesson 6-7

--- lesson-06-js-syntax/level-07-declare-objects/script.js
+++ lesson-06-js-syntax/level-07-declare-objects/script.js
@@ -1,9 +1,61 @@
 // Level 07 — Declare 10 objects with properties
 // TODO: Declare and initialize 10 object variables with >=3 properties each.
 
+import isLoggedIn from "../level-04-booleans-variables/script";
+
 // Example:
 // let userProfile = { name: "Ava", age: 28, isSubscriber: false };
 
-let userProfile = {};
+let userProfile = {
+    name: Joseph, 
+    Age:5000, 
+    isSubsriber:true
+};
+let userName = {
+    name: Joseph, 
+    newUser: true, 
+    isLoggedIn = true
+};
+let firstName = {
+    name1: "Joseph",
+    name2: "Xeroc",
+    name3: "Wrinn"
+};
+let lastName = {
+    name1: "M",
+    name2: "Astrum",
+    name3: "Holdover"
+};
+let midName= {
+    name1: "X",
+    name2: "Deus",
+    name3: "Varian"
+};
+let Age = {
+    birthYear: 0, 
+    birthMonth: "January", 
+    birthDate:1
+};
+let birthDate= {
+    isOld: true,
+    isYoung: false,
+    Age: 5000
+};
+let currentDate= {
+    Month: "April",
+    currentDay: 28,
+    currentYear: 2026
+};
+let Gender={
+    isGender: true,
+    isntGender: true,
+    makesSense: false
+};
+let Random = {
+    Name: 5000,
+    Age: "Joseph",
+    isRandom: false
+}
+
 
 export default userProfile;
