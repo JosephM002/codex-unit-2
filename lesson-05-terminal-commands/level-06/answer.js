d0082af4bc00a7f69be9206ddc0ec0f1f1324b1c
Joseph <corrymuth002@gmail.com>
2026-04-29 20:31:33 -0500

    modified lesson 5-6

--- lesson-05-terminal-commands/level-06/answer.js
+++ lesson-05-terminal-commands/level-06/answer.js
@@ -1,3 +1,5 @@
 // Level 06 — Run Node.js files with node
 // TODO: Run `node hello.js` and write a short note here mentioning `node`.
-export default "";
+export default ["node", "hello.js"];
+console.log('test');
+export default "ran current js file"
