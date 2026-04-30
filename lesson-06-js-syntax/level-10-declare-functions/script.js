ef90ba6b927c68ac537b5ec87d1c63d1cc02a798
Joseph <corrymuth002@gmail.com>
2026-04-29 20:34:20 -0500

    modified lesson 6-10

--- lesson-06-js-syntax/level-10-declare-functions/script.js
+++ lesson-06-js-syntax/level-10-declare-functions/script.js
@@ -4,3 +4,31 @@
 function getUser() {
   return "returns the current user";
 }
+function firstFunction() {
+  return "An example function";
+}
+function getAge() {
+  return "gets the age of the current user";
+}
+function returnText() {
+  return "returns this text";
+}
+function getPassword() {
+  return "returns the password of the current user";
+}
+function isTrue() {
+  return "this function is false";
+}
+function isLoggedIn() {
+  return "shows if user is currently logged in";
+}
+function lastFunction() {
+  return "this is the last function in the list";
+}
+function isOnline() {
+  return "shows if user currently online";
+}
+function logOut() {
+  return "this function logs out user";
+}
+export default getUser;
