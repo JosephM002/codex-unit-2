4ac0967a456208bdd51dd98e61776fe2dec3c53e
Joseph <corrymuth002@gmail.com>
2026-05-01 18:43:53 -0500

    explained Get requests vs Post requests

--- lesson-08-rest-api/level-08/script.js
+++ lesson-08-rest-api/level-08/script.js
@@ -2,3 +2,5 @@
 // TODO: Save a target element and set its innerText to briefly compare GET and POST.
 const result = document.getElementById("result");
 // Example: result.innerText = 'GET is for reading; POST is for sending data.'
+result.innerText =
+  "GET requests only read and interpret incoming data, while POST requests may change it";
