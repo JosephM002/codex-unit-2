ac9a3b63e8a47f58fc8ac62217d763a344ed0e8e
Joseph <corrymuth002@gmail.com>
2026-05-01 18:42:42 -0500

    explained fetch command

--- lesson-08-rest-api/level-04/script.js
+++ lesson-08-rest-api/level-04/script.js
@@ -6,6 +6,9 @@ const result = document.getElementById("result");
 async function handleSubmit(event) {
   event.preventDefault();
   // TODO: fetch and parse JSON, then set result.innerText with a short explanation
+  const waiting = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await waiting.json();
+  result.innerText = "Converts incoming data to a js object";
 }
 
 if (form) form.onsubmit = handleSubmit;
