c365a378640ce36f365c2e9b6918c193e349d62a
Joseph <corrymuth002@gmail.com>
2026-05-01 18:41:31 -0500

    explained async and await

--- lesson-08-rest-api/level-03/script.js
+++ lesson-08-rest-api/level-03/script.js
@@ -1,11 +1,13 @@
 // Level 03 starter script
 // TODO: Use async/await when calling fetch inside the submit handler.
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
+if (form) form.onsubmit = handleSubmit;
 
 async function handleSubmit(event) {
   event.preventDefault();
   // TODO: use async/await to call fetch('https://postman-echo.com/get') and then set result.innerText
+  const waiting = await fetch("https://jsonplaceholder.typicode.com");
+  const result = document.getElementById("result");
+  result.innerText =
+    "async/await causes the code to wait until a process occurs to perform an action";
 }
-
-if (form) form.onsubmit = handleSubmit;
