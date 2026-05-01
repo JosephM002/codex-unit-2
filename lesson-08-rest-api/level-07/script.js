3d95f363e3f69e550c819d730e5438023138967f
Joseph <corrymuth002@gmail.com>
2026-05-01 18:43:35 -0500

    explained post requests

--- lesson-08-rest-api/level-07/script.js
+++ lesson-08-rest-api/level-07/script.js
@@ -7,6 +7,15 @@ async function handleSubmit(e) {
   e.preventDefault();
   // TODO: collect values via form.elements[0].value, form.elements[1].value, form.elements[2].value
   // build a data object and post it with fetch; then set result.innerText explaining how data is attached
+  const data = {
+    name: form.elements[0].value,
+    email: form.elements[1].value,
+    note: form.elements[2].value,
+  };
+  const waiting = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const received = await waiting.json();
+  result.innerText =
+    "Data is attached to a post request by sending it with a json";
 }
 
 if (form) form.onsubmit = handleSubmit;
