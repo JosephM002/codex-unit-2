a0f438cfb418f211bf7ac55075c1eb0d85ec46d3
Joseph <corrymuth002@gmail.com>
2026-04-30 19:28:27 -0500

    described rest APIs

--- lesson-08-rest-api/level-01/script.js
+++ lesson-08-rest-api/level-01/script.js
@@ -2,3 +2,4 @@
 // TODO: Select a target element and set its innerText to explain REST API.
 const result = document.getElementById("result");
 // Example: result.innerText = '...'; // fill this in during the exercise
+result.innerText = "Rest APIs take client requests and return responses.";
