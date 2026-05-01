b43461910494069bcb5bdc1ae8946de123d34732
Joseph <corrymuth002@gmail.com>
2026-05-01 18:43:03 -0500

    implemented fetch and innerText

--- lesson-08-rest-api/level-05/script.js
+++ lesson-08-rest-api/level-05/script.js
@@ -8,6 +8,14 @@ const pEnc = document.getElementById("encoding");
 async function handleSubmit(e) {
   e.preventDefault();
   // TODO: implement fetch and set the three elements' innerText
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const userdata = await response.json();
+  const found = userdata.userId;
+  const headers = userdata.title;
+  const encode = userdata.completed;
+  pUrl.innerText = found;
+  pHost.innerText = headers;
+  pEnc.innerText = encode;
 }
 
 if (form) form.onsubmit = handleSubmit;
