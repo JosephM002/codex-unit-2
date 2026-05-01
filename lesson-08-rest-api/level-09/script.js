877f90005e6af86cd4c4a0b7370ce218c27a4402
Joseph <corrymuth002@gmail.com>
2026-05-01 18:44:23 -0500

    parsed response and inserted value

--- lesson-08-rest-api/level-09/script.js
+++ lesson-08-rest-api/level-09/script.js
@@ -8,6 +8,19 @@ const v3 = document.getElementById("v3");
 async function handleSubmit(e) {
   e.preventDefault();
   // TODO: collect data, POST to echo server, parse response, and insert values into v1/v2/v3
+  const waiting = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const recieved = waiting.json();
+  const a = form.elements[0].value;
+  const b = form.elements[1].value;
+  const c = form.elements[2].value;
+  const data = {
+    a: form.elements[0].value,
+    b: form.elements[1].value,
+    c: form.elements[2].value,
+  };
+  v1.innerText = a;
+  v2.innerText = b;
+  v3.innerText = c;
 }
 
 if (form) form.onsubmit = handleSubmit;
