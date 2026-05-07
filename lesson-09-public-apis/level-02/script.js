566777c727f686c0eae9ba7db03c4fe2243121d6
Joseph <corrymuth002@gmail.com>
2026-05-06 19:01:53 -0500

    updated js for lesson 9 lvl 2

--- lesson-09-public-apis/level-02/script.js
+++ lesson-09-public-apis/level-02/script.js
@@ -1,12 +1,32 @@
 const form2 = document.getElementById("login-form");
 const errorEl2 = document.getElementById("error");
 const successEl2 = document.getElementById("success");
+if (form2) form2.onsubmit = handleSubmit;
+let progress = 0;
 
-if (form2) {
-  form2.addEventListener("submit", async (e) => {
-    // TODO: prevent default, call fetch/login flow
-    // TODO: update errorEl2 or successEl2 depending on response
-    // On success, call form.reset() per the tasks
-    console.log("level-02 submit placeholder");
+async function handleSubmit(event) {
+  event.preventDefault();
+  const formTag = event.target;
+  const data = {
+    username: formTag.elements.username.value,
+    password: formTag.elements.password.value,
+  };
+  const dataString = JSON.stringify(data);
+  const waiting = await fetch("https://dummyjson.com/auth/login", {
+    method: "POST",
+    body: dataString,
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
+  const receive = await waiting.json();
+  const message = receive.message;
+  const firstName = receive.firstName;
+  if (message) {
+    const errorTag = document.getElementById("error");
+    errorTag.innerText = message;
+  } else if (firstName) {
+    const successTag = document.getElementById("success");
+    successTag.innerText = "Logged";
+  }
 }
