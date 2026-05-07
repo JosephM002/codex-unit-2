f6ea4ff7443aafea12eb7ea45165939e4dfd9734
Joseph <corrymuth002@gmail.com>
2026-05-06 19:02:57 -0500

    updated js for lesson 9 lvl 8

--- lesson-09-public-apis/level-08/script.js
+++ lesson-09-public-apis/level-08/script.js
@@ -2,10 +2,20 @@ const btn8 = document.getElementById("get-joke");
 const jokeEl = document.getElementById("joke");
 const errorEl8 = document.getElementById("error");
 
-if (btn8) {
-  btn8.addEventListener("click", async () => {
-    // TODO: perform fetch sending API key in header or query param as required
-    // TODO: parse response and render joke string, handle invalid key errors
-    console.log("level-08 placeholder");
-  });
+async function handleSubmit(event) {
+  event.preventDefault();
+  const data = {
+    "api-key": "123456789009876543321a",
+  };
+  const queryString = new URLSearchParams(data);
+  const waiting = await fetch(
+    "https://humorapi.com/memes/random" + "?" + queryString,
+  );
+  const received = await waiting.json();
+  const description = received.description;
+  const src = received.url;
+  const imgTag = getElementById("meme");
+  const outputTag = getElementById("description");
+  outputTag.innerText = description;
+  imgTag.src = src;
 }
