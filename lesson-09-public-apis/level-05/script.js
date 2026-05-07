f4778540c4fba621df693925b83cd142a1c51462
Joseph <corrymuth002@gmail.com>
2026-05-06 19:02:25 -0500

    updated js for lesson 9 lvl 5

--- lesson-09-public-apis/level-05/script.js
+++ lesson-09-public-apis/level-05/script.js
@@ -1,11 +1,22 @@
 const triviaForm = document.getElementById("trivia-form");
 const questionEl = document.getElementById("question");
+triviaForm.onsubmit = trivia;
 
-if (triviaForm) {
-  triviaForm.addEventListener("submit", async (e) => {
-    // TODO: preventDefault, build query params with URLSearchParams
-    // TODO: fetch trivia API with async/await then parse JSON
-    // TODO: render result[0].question.text into the page
-    console.log("level-05 placeholder");
-  });
+async function trivia(event) {
+  event.preventDefault();
+  const data = {
+    category: triviaForm.elements.category.value,
+    difficulty: triviaForm.elements.difficulty.value,
+  };
+  // TODO: preventDefault, build query params with URLSearchParams
+  // TODO: fetch trivia API with async/await then parse JSON
+  // TODO: render result[0].question.text into the page
+  const param = new URLSearchParams(data);
+  const waiting = await fetch(
+    "https://the-trivia-api.com/v2/questions" + "?" + param,
+  );
+  const received = await waiting.json();
+  console.log(received);
+  questionEl.innerText = received[0].question.text;
+  console.log("level-05 placeholder");
 }
