45903f82a235c19186c069dd0754cc5cdeeca725
Joseph <corrymuth002@gmail.com>
2026-05-06 19:02:47 -0500

    updated js for lesson 9 lvl 7

--- lesson-09-public-apis/level-07/script.js
+++ lesson-09-public-apis/level-07/script.js
@@ -2,5 +2,5 @@ const msg7 = document.getElementById("message");
 if (msg7) {
   // TODO: insert explanatory text about API keys (do not include real keys)
   msg7.textContent =
-    "Add an explanation about API keys here. Do not commit real keys.";
+    "An Api key is a string that APIs use as a code while communicating. They help id users and improve security of the site";
 }
