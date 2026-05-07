05e8a9bfad6e5fe1eb8cfef97a44d5c2bad6513a
Joseph <corrymuth002@gmail.com>
2026-05-06 19:02:14 -0500

    updated js for lesson 9 lvl 4

--- lesson-09-public-apis/level-04/script.js
+++ lesson-09-public-apis/level-04/script.js
@@ -3,6 +3,13 @@ const out4 = document.getElementById("out");
 
 if (form4) {
   form4.addEventListener("submit", (e) => {
+    const data = {
+      category: asd,
+      difficulty: asf,
+    };
+    const params = new URLSearchParams(data);
+    console.log("this is the string" + params);
+    fetch(params);
     // TODO: preventDefault, build a data object, use URLSearchParams
     // TODO: append query string to GET request URL
     console.log("level-04 submit placeholder");
