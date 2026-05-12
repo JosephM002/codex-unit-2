223600ad43299fdd7c17d681a163a8b15bb84e69
Joseph <corrymuth002@gmail.com>
2026-05-11 20:38:52 -0500

    updated lesson 8 lvl 7 js

--- lesson-08-rest-api/level-07/script.js
+++ lesson-08-rest-api/level-07/script.js
@@ -5,6 +5,17 @@ const result = document.getElementById("result");
 
 async function handleSubmit(e) {
   e.preventDefault();
+  const form = event.target;
+  const data = {
+    username: form.elements.username.value,
+    email: form.elements.email.value,
+  };
+  const response = await fetch("https://api.jsoning.com/mock/public/users", {
+    method: "POST",
+    body: data,
+  });
+  result.innerText =
+    "the data object saved the input of the form, then when the site was fetched, called";
   // TODO: collect values via form.elements (e.g. form.elements['username'].value)
   // Build a `data` object whose keys match the input `name` attributes.
   // NOTE: intentionally do NOT JSON.stringify the data here; attach the raw object to `body` to expose an error for students to debug.
