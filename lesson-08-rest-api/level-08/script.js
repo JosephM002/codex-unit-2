729a59c890789a1598bf61e3d008bbccd697b232
Joseph <corrymuth002@gmail.com>
2026-05-11 20:38:59 -0500

    updated lesson 8 lvl 8 js

--- lesson-08-rest-api/level-08/script.js
+++ lesson-08-rest-api/level-08/script.js
@@ -5,6 +5,19 @@ const result = document.getElementById("result");
 
 async function handleSubmit(e) {
   e.preventDefault();
+  const form = event.target;
+  const data = {
+    username: form.elements.username.value,
+    email: form.elements.email.value,
+  };
+  const dataString = JSON.stringify(data);
+  const response = await fetch("https://api.jsoning.com/mock/public/users", {
+    method: "POST",
+    headers: { "Content-type": "application/JSON" },
+    body: dataString,
+  });
+  result.innerText =
+    "data is saved in the data constant. turned to a string with stringify, then attached to the body when fetch";
   // TODO: collect values via form.elements[0].value, form.elements[1].value, form.elements[2].value
   // build a data object and post it with fetch; then set result.innerText explaining how data is attached
 }
