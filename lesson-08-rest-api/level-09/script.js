a9dafed8726f76949efc93d4585f26b8f97a2f82
Joseph <corrymuth002@gmail.com>
2026-05-11 20:39:07 -0500

    updated lesson 8 lvl 9 js

--- lesson-08-rest-api/level-09/script.js
+++ lesson-08-rest-api/level-09/script.js
@@ -2,3 +2,5 @@
 // TODO: Save a target element and set its innerText to briefly compare GET and POST.
 const result = document.getElementById("result");
 // Example: result.innerText = 'GET is for reading; POST is for sending data.'
+result.innerText =
+  "GET only gets info, but POST will also send data of its own";
