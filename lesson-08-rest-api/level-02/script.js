da5bed904212c3dcf0508d55199bc4207f94ab3c
Joseph <corrymuth002@gmail.com>
2026-05-11 20:38:17 -0500

    updated lesson 8 lvl 2 js

--- lesson-08-rest-api/level-02/script.js
+++ lesson-08-rest-api/level-02/script.js
@@ -6,6 +6,7 @@ const result = document.getElementById("result");
 function handleSubmit(event) {
   event.preventDefault();
   // TODO: set result.innerText to a short explanation about fetch
+  const response = fetch("https://api.jsoning.com/mock/public/users");
   result.innerText = "fetch makes a network request to resolve a response";
 }
 
