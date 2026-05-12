6762a8c2e1bd21c8516b26140450a4cff0abaee5
Joseph <corrymuth002@gmail.com>
2026-05-11 20:41:21 -0500

    updated lesson 8 lvl 11 js

--- lesson-08-rest-api/level-11/script.js
+++ lesson-08-rest-api/level-11/script.js
@@ -7,12 +7,26 @@ const r3 = document.getElementById("r3");
 
 async function handleSubmit(e) {
   e.preventDefault();
-  const response = await fetch("https://api.jsoning.com/mock/public/users", {
+  const form = event.target;
+  const data = {
+    one: form.elements.one.value,
+    two: form.elements.two.value,
+    two: form.elements.three.value,
+  };
+  const dataString = JSON.stringify(data);
+  const response = await fetch("https://api.jsoning.com/mock/public/products", {
     method: "POST",
     headers: { "Content-type": "application/JSON" },
-    body: JSON.stringify(),
+    body: dataString,
   });
   // TODO: build data object, POST it, parse response, and display three response values
 }
+const test = response.JSON;
+const r11 = test.body;
+const r22 = test.username;
+const r33 = test.password;
+r1.innerText = r11;
+r2.innerText = r22;
+r3.innerText = r33;
 
 if (form) form.onsubmit = handleSubmit;
