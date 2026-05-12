4f16e6f3b0adbac5889999480400f619c09efcea
Joseph <corrymuth002@gmail.com>
2026-05-11 20:39:14 -0500

    updated lesson 8 lvl 10 js

--- lesson-08-rest-api/level-10/script.js
+++ lesson-08-rest-api/level-10/script.js
@@ -7,6 +7,19 @@ const v3 = document.getElementById("v3");
 
 async function handleSubmit(e) {
   e.preventDefault();
+  const response = await fetch("https://api.jsoning.com/mock/public/users", {
+    method: "POST",
+    headers: { "Content-type": "application/JSON" },
+    body: JSON.stringify(),
+  });
+  const body = await response.json;
+  const v11 = body.json;
+  v1.innerText = v11;
+  const v22 = body.username;
+  v2.innerText = v22;
+  const v33 = body.password;
+  v3.innerText = v33;
+
   // TODO: collect data, POST to echo server, parse response, and insert values into v1/v2/v3
 }
 
