3cc3103ca8a877d90d60eadb216596c1705b4840
Joseph <corrymuth002@gmail.com>
2026-05-13 18:21:06 -0500

    updates lesson 9 level 4 js

--- lesson-09-public-apis/level-04/script.js
+++ lesson-09-public-apis/level-04/script.js
@@ -3,6 +3,14 @@ formTag.onsubmit = handleSubmit;
 
 function handleSubmit(event) {
   event.preventDefault();
+  const form = event.target;
+  const data = {
+    category: formTag.elements.category.value,
+    difficulty: formTag.elements.difficulty.value,
+  };
+  const params = new URLSearchParams(data);
+  const query = params.toString();
+  console.log(query);
   // TODO: Read form values with formTag.elements and create `data` object
   // TODO: Use `new URLSearchParams(data)` to create a query string and save to `query`
   // TODO: console.log(query)
