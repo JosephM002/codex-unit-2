6261326860299478c4b934664be12816c5380ee8
Joseph <corrymuth002@gmail.com>
2026-05-13 18:21:47 -0500

    updates lesson 9 level 9 js

--- lesson-09-public-apis/level-09/script.js
+++ lesson-09-public-apis/level-09/script.js
@@ -1,14 +1,28 @@
 const formTag = document.getElementById("challengeForm");
 formTag.onsubmit = handleSubmit;
+const err = document.getElementById("error");
+const suc = document.getElementById("success");
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
+  try {
+    const form = event.target;
+    const data = {
+      name: formTag.elements.value.value,
+    };
+    const dataString = JSON.stringify(data);
+    console.log(dataString);
+    const response = await fetch("https://humorapi.com/memes/random", {});
+    const result = await response.json;
+    const suc1 = suc;
+    suc1.innerText = "logging in!";
+  } catch {
+    console.log("error");
+    const err1 = err;
+    err1.innerText = "Theres been an error";
+  }
   // TODO: Construct `data` from formTag.elements
   // TODO: Format data with `URLSearchParams` or `JSON.stringify`
   // TODO: Save formatted data into `dataString` and send via fetch using async/await
   // TODO: Parse `result` and update the page; handle errors with try/catch
-}
-
-async function handleSubmit(event) {
-  event.preventDefault();
 }
