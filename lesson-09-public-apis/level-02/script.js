28820da2d2dd86d727f1a916d56c6f25c2f8b36b
Joseph <corrymuth002@gmail.com>
2026-05-13 18:20:50 -0500

    updates lesson 9 level 2 js

--- lesson-09-public-apis/level-02/script.js
+++ lesson-09-public-apis/level-02/script.js
@@ -1,8 +1,21 @@
 const formTag = document.getElementById("loginForm");
 formTag.onsubmit = handleSubmit;
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
+  const form = event.target;
+  const data = {
+    username: form.elements.username.value,
+    password: form.elements.password.value,
+  };
+  const dataString = JSON.stringify(data);
+  const response = await fetch("https://dummyjson.com/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: dataString,
+  });
+  const result = await response.json;
+  console.log(result);
   // TODO: Build `data` object from `form.elements` (username, password)
   // TODO: Use async/await and fetch to POST to https://dummyjson.com/auth/login
   // TODO: Include headers: { 'Content-Type': 'application/json' }
